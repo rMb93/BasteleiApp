c01b793c9cca153813a17fa399f8553a09d93c36
Robin <robin.bauknecht@gmail.com>
2015-11-15 14:19:06 +0100

    Initial Commit

---
+++ UnitTestProject1/Test.feature
@@ -0,0 +1,17 @@
+﻿Feature: VisualizeData
+	In order to see the weather data
+	As a user
+	I want to see the collected data
+
+@changeLocation
+Scenario: Change location of shown data
+	Given I see the location selection menu
+	When I click on the location "toilet"
+	Then the data of "toilet" should be shown
+
+@changeTimeframe
+Scenario: Change timeframe of shown data
+	Given I have selected the location "toilet"
+	When I click on the ComboBox arrow
+	And I click on the timeframe "last week"
+	Then the data of the last week is shown
---
+++ VisualizeDataSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace UnitTestProject1...
+{
+    [Binding]
+    public class VisualizeDataSteps
+    {
+        [Given(@"I see the location selection menu")]
+        public void GivenISeeTheLocationSelectionMenu()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have selected the location ""(.*)""")]
+        public void GivenIHaveSelectedTheLocation(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on the location ""(.*)""")]
+        public void WhenIClickOnTheLocation(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on the ComboBox arrow")]
+        public void WhenIClickOnTheComboBoxArrow()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on the timeframe ""(.*)""")]
+        public void WhenIClickOnTheTimeframe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the data of ""(.*)"" should be shown")]
+        public void ThenTheDataOfShouldBeShown(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the data of the last week is shown")]
+        public void ThenTheDataOfTheLastWeekIsShown()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
